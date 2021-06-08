0f67a5f389959b0c38f74208d495d5b0ba602cee
guichezero@mail.com <srbillgithub96>
2021-06-08 03:00:31 -0300

    wudao

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 http://wudaoai.cn/ wudao2.0 ai fake new generator public api consumption
+
+experimental, i still do not fully indestand the api.
--- fake_new_generator.py
+++ fake_new_generator.py
@@ -6,6 +6,9 @@ __author__ = 'hwpoison'
 
 session = requests.Session()
 
+API_CLIENT_ID = ""
+API_SECRET_KEY = ""
+
 def post_request(url, payload, headers={'Content-type': 'application/json'}):
     try:
         req = session.post(url, data=json.dumps(payload), verify=True, headers=headers)
@@ -14,6 +17,38 @@ def post_request(url, payload, headers={'Content-type': 'application/json'}):
     except requests.exceptions.RequestException as err:
         print("Oops!:", err)
         return False
+
+def getBaaiApiToken():
+    auth_url = "https://wudaoai.cn/model-api/api/v1/auth"
+    auth_data = {
+        "clientId":API_CLIENT_ID,
+        "secretKey":API_SECRET_KEY    
+    }
+    req = post_request(auth_url, auth_data)
+    return req['data']['apiToken']
+
+def vectorText(text):
+    token = getBaaiApiToken() 
+    data = {
+        'file':'',
+        'baaiApiToken':token,
+        'json':{"text":text}
+    }
+    api_txtvect = "https://wudaoai.cn/model-api/api/v1/verifyApi"
+    req = post_request(api_txtvect, data, {'Content-type':'form-data'})
+    return req['data']
+
+def poems_generator(content):
+    # input format : <title>&&<author>&&<poem_text>
+    api_poems_url = "https://wudao.aminer.cn/api/fastpoem/"
+    data = {
+        'content':content
+    }
+    out = post_request(api_poems_url, data)
+    if out:
+        return out["result"]
+    else:
+        return "Error."
 
 def fakenew_generator(content, length=10000):
     api_news_url = "https://wudao.aminer.cn/api/xinhua/"
@@ -26,6 +61,7 @@ def fakenew_generator(content, length=10000):
         return out["result"]
     else:
         return "Error."
+
 
 while True:
     inpt = input("Input:")
